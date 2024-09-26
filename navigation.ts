2556b4549c27707db9d8a75b66656c46afc3ee38
戴富阳 <1140444693@qq.com>
2024-09-26 14:52:47 +0000

    新增公共页脚组件

---
+++ navigation.ts
@@ -0,0 +1,37 @@
+export default [
+    {
+        name: "首页",
+        href: "/",
+        target: "_self"
+    },
+    {
+        name: "关于我们",
+        href: "/about",
+        target: "_self"
+    },
+    {
+        name: "服务项目",
+        href: "/services",
+        target: "_self"
+    },
+    {
+        name: "案例展示",
+        href: "/portfolio",
+        target: "_self"
+    },
+    {
+        name: "客户评价",
+        href: "/testimonials",
+        target: "_self"
+    },
+    {
+        name: "博客分享",
+        href: "/blog",
+        target: "_self"
+    },
+    {
+        name: "联系我们",
+        href: "/contact",
+        target: "_self"
+    }
+];
