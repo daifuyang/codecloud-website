71d7d913146e972620051763da02183b884192fa
戴富阳 <1140444693@qq.com>
2024-09-28 21:55:19 +0800

    完成首页代码

--- navigation.ts
+++ navigation.ts
@@ -1,32 +1,32 @@
 export default [
-    {
-        name: "首页",
-        href: "/",
-        target: "_self"
-    },
-    {
-        name: "关于我们",
-        href: "/about",
-        target: "_self"
-    },
-    {
-        name: "服务项目",
-        href: "/services",
-        target: "_self"
-    },
-    {
-        name: "案例展示",
-        href: "/portfolio",
-        target: "_self"
-    },
-    {
-        name: "博客分享",
-        href: "/blog",
-        target: "_self"
-    },
-    {
-        name: "联系我们",
-        href: "/contact",
-        target: "_self"
-    }
+  {
+    name: "首页",
+    href: "/",
+    target: "_self",
+  },
+  {
+    name: "关于我们",
+    href: "/about",
+    target: "_self",
+  },
+  {
+    name: "服务项目",
+    href: "/services",
+    target: "_self",
+  },
+  {
+    name: "案例展示",
+    href: "/portfolio",
+    target: "_self",
+  },
+  {
+    name: "博客分享",
+    href: "/blog",
+    target: "_self",
+  },
+  {
+    name: "联系我们",
+    href: "/contact",
+    target: "_self",
+  },
 ];
